41ddd599f52f722bde9fe783c97d6790adb0eb82
LAPTOP-FTRF3I3B\Paulo Campos <paulo.campos@basecone.nl>
2017-01-27 11:31:22 +0000

    This is a test

---
+++ Tests/Tests/Guard.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests
+{
+    using System;
+
+    public static class Guard
+    {
+        public static void Against<T>(bool condition) where T : Exception, new()
+        {
+            if (!condition)
+            {
+                var instance = (T)Activator.CreateInstance(typeof(T));
+                throw instance;
+            }
+        }
+
+        public static void Against<T>(bool condition, string message) where T : Exception, new()
+        {
+            if (!condition)
+            {
+                var instance = (T)Activator.CreateInstance(typeof(T), new object[] { message });
+                throw instance;
+            }
+        }
+
+        public static void Against<T>(bool condition, string message, Exception innerException) where T : Exception, new()
+        {
+            if (!condition)
+            {
+                var instance = (T)Activator.CreateInstance(typeof(T), new object[] { message, innerException });
+                throw instance;
+            }
+        }
+    }
+}
---
+++ Tests/Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guard.Against<NullReferenceException>(1 == 3, "Error 1");
+            Guard.Against<DivideByZeroException>(1 == 2, "Error");
+        }
+    }
+}
