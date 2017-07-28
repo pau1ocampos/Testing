d272b2aeb40185ce885e02c29fd56d624e6692c0
paulo.campos <paulo.campos@basecone.nl>
2017-07-28 10:31:20 +0100

    Added .NET core project

---
+++ GuardNetCore/src/GuardCore/Guard.cs
@@ -0,0 +1,39 @@
+﻿namespace GuardCore
+{
+    using System;
+    public static class Guard
+    {
+        public static void Evaluate<T>(bool condition) where T : Exception, new()
+        {
+            if (!condition)
+            {
+                return;
+            }
+
+            var instance = (T)Activator.CreateInstance(typeof(T));
+            throw instance;
+        }
+
+        public static void Evaluate<T>(bool condition, string message) where T : Exception, new()
+        {
+            if (!condition)
+            {
+                return;
+            }
+
+            var instance = (T)Activator.CreateInstance(typeof(T), message);
+            throw instance;
+        }
+
+        public static void Evaluate<T>(bool condition, string message, Exception innerException) where T : Exception, new()
+        {
+            if (!condition)
+            {
+                return;
+            }
+
+            var instance = (T)Activator.CreateInstance(typeof(T), message, innerException);
+            throw instance;
+        }
+    }
+}
---
+++ GuardNetCore/src/GuardCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuardCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Guard.Evaluate<DivideByZeroException>(true, "Cannot divide by zero.");
+        }
+    }
+}
